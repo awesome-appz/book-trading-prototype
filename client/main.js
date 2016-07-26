3e5ae3c22705e12f33c0b10f76309eb8956042e7
Arijit Layek <alayek@engineer.com>
2016-07-26 12:56:30 +0000

    Add react routes

--- client/main.js
+++ client/main.js
@@ -1,4 +1,12 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+// import './main.html';
+
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom'
+import { renderRoutes } from './routes.js'
+
+Meteor.startup( () => {
+    render(renderRoutes(), document.getElementById("render-target"))
+})
